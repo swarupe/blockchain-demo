66deb84b4b3ac30d288721f1c941aeae7c5a079b
swarupe <swarup.e1998@gmail.com>
2018-09-08 16:12:48 +0530

    Init api.js

--- dev/test.js
+++ dev/test.js
@@ -2,10 +2,4 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2321, 'SGHDJGYETUISGJG', 'SHKFJSHKFJHSKDJSH');
-
-bitcoin.createNewTransaction(100, 'ALEXDJHGHSG7987SAD','JENNSHDJHDJ87987SB')
-
-bitcoin.createNewBlock(123456, 'sfdsgdg', 'SHKFJSHKFJHSdsgsdgsdgKDJSH');
-
 console.log(bitcoin);
