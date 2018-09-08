4c8484490c1493875568185473f822a042bbc6b5
swarupe <swarup.e1998@gmail.com>
2018-09-08 15:36:26 +0530

    added blockchain data structure

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,7 +3,9 @@ const sha256 = require('sha256');
 function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
-}
+
+    this.createNewBlock(100, '0', '0');
+};
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
     const newBlock = {
@@ -19,11 +21,11 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
     this.chain.push(newBlock);
 
     return newBlock;
-}
+};
 
 Blockchain.prototype.getLastBlock = function() {
     return this.chain[this.chain.length - 1];
-}
+};
 
 Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
     const newTransaction = {
@@ -35,12 +37,22 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) 
     this.pendingTransactions.push(newTransaction);
 
     return this.getLastBlock()['index'] + 1;
-}
+};
 
 Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
     const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
     const hash = sha256(dataAsString);
     return hash;
-}
+};
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while (hash.substring(0,4) !== '0000') {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+    return nonce;
+};
 
 module.exports = Blockchain;
